957128b2e86a6795e747aaf52f019649d2f170e5
Andres Pinilla Palacios <apinillapalacios@gmail.com>
2024-07-29 11:00:53 -0500

    test

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -12,6 +12,8 @@ import { Session } from '@/lib/types'
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = 'force-dynamic';
 
+//
+
 export interface ChatPageProps {
   params: {
     id: string
